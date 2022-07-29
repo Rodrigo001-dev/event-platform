b4017d8049d4498af3bfcfff6b1cb8244ea1103e
Rodrigo <rodrigorael53@gmail.com>
2022-07-29 05:20:41 -0300

    adding hover event on links

--- src/components/Video.tsx
+++ src/components/Video.tsx
@@ -20,12 +20,13 @@ export function Video() {
           </div>
 
           <div className="flex flex-col gap-4">
-            <a href="" className="p-4 text-sm bg-green-500 flex items-center rounded font-bold uppercase gap-2 justify-center">
+            {/* hover:bg-green-700 é para quando o usuário estiver com o mouse em cima do link e escurecer, transition-colors para fazer uma transição mais suave */}
+            <a href="" className="p-4 text-sm bg-green-500 flex items-center rounded font-bold uppercase gap-2 justify-center hover:bg-green-700 transition-colors">
               <DiscordLogo size={24} />
               Comunidade do Discord
             </a>
 
-            <a href="" className="p-4 text-sm border border-blue-500 text-blue-500 flex items-center rounded font-bold uppercase gap-2 justify-center">
+            <a href="" className="p-4 text-sm border border-blue-500 text-blue-500 flex items-center rounded font-bold uppercase gap-2 justify-center hover:bg-blue-500 hover:text-gray-900 transition-colors">
               <Lightning size={24} />
               Acesse o desafio
             </a>
